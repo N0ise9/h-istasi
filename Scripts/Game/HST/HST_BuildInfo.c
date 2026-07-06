45a9508d59b802f6896a523ed126ef0b566efd04
Austin Baker <noise.9no@gmail.com>
2026-07-06 09:19:38 -0400

    Bump runtime proof stamp to r15

--- Scripts/Game/HST/HST_BuildInfo.c
+++ Scripts/Game/HST/HST_BuildInfo.c
@@ -1,8 +1,8 @@
 class HST_BuildInfo
 {
-	static const string BUILD_SHA = "22ca457+uirenderproof";
+	static const string BUILD_SHA = "21521be+gm-captive-proof";
 	static const string BUILD_UTC = "2026-07-06T13:05:16Z";
-	static const string BUILD_LABEL = "h-istasi-live-runtime-proof-r14-ui-render-proof";
+	static const string BUILD_LABEL = "h-istasi-live-runtime-proof-r15-gm-captive-proof";
 
 	static string BuildSummary()
 	{
