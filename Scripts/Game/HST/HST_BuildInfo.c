c773dc2c92cb2bb644932895e90eebab86265cc0
Austin Baker <noise.9no@gmail.com>
2026-07-15 18:29:04 -0400

    Stamp hardened counterattack restart proof build

--- Scripts/Game/HST/HST_BuildInfo.c
+++ Scripts/Game/HST/HST_BuildInfo.c
@@ -1,7 +1,7 @@
 class HST_BuildInfo
 {
-	static const string BUILD_SHA = "d91a0ff88d6d3da44b69947559f95d1c54ff91bb";
-	static const string BUILD_UTC = "2026-07-15T19:13:20Z";
+	static const string BUILD_SHA = "e596d986a1c85ddffeee414fee4c6c3d3559c29f";
+	static const string BUILD_UTC = "2026-07-15T22:28:37Z";
 	static const string BUILD_LABEL = "schema70-settings24-counterattack-virtual-restart-proof";
 
 	static string BuildSummary()
