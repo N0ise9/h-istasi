73bfccf630c436b2c4acb90e8b80645b1ec4a586
Austin Baker <noise.9no@gmail.com>
2026-07-15 12:34:43 -0400

    Stamp follow-up owner admission checkpoint

--- Scripts/Game/HST/HST_BuildInfo.c
+++ Scripts/Game/HST/HST_BuildInfo.c
@@ -1,7 +1,7 @@
 class HST_BuildInfo
 {
-	static const string BUILD_SHA = "446f5e4e756091531d3b540283d775ed1947cd05";
-	static const string BUILD_UTC = "2026-07-15T16:13:35Z";
+	static const string BUILD_SHA = "7c8b9c27b4ee553664fa2b44aea4a8d53c7123a5";
+	static const string BUILD_UTC = "2026-07-15T16:34:25Z";
 	static const string BUILD_LABEL = "schema70-settings24-phase24-owner-snapshot";
 
 	static string BuildSummary()
