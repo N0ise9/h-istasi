3a15f7718f929e0fe2e830f4668d4674ae8388cb
Austin Baker <noise.9no@gmail.com>
2026-07-15 21:10:11 -0400

    Stamp dematerializing counterattack restart proof

--- Scripts/Game/HST/HST_BuildInfo.c
+++ Scripts/Game/HST/HST_BuildInfo.c
@@ -1,8 +1,8 @@
 class HST_BuildInfo
 {
-	static const string BUILD_SHA = "339b72ec3ed63132e46f3df84540d74d3e938d16";
-	static const string BUILD_UTC = "2026-07-15T22:32:02Z";
-	static const string BUILD_LABEL = "schema70-settings24-counterattack-virtual-restart-proof";
+	static const string BUILD_SHA = "87a4ae2491ec5b83d37dbc43e1658f3380bb8b1c";
+	static const string BUILD_UTC = "2026-07-16T01:06:29Z";
+	static const string BUILD_LABEL = "schema70-settings24-counterattack-dematerializing-restart-proof";
 
 	static string BuildSummary()
 	{
